018fb66aeab60a68676fe45cecda048dc5799b42
kartik mathur <kartik.mathur10@gmail.com>
2022-04-17 14:55:10 +0530

    Lecture-31

--- Lecture-31/BinaryTrees.cpp
+++ Lecture-31/BinaryTrees.cpp
@@ -1,5 +1,6 @@
 // BinaryTrees.cpp
 #include <iostream>
+#include <queue>
 using namespace std;
 // 8 10 1 -1 -1 6 4 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
 class node {
@@ -117,11 +118,113 @@ Pair fastDiameter(node* root) {
 	return ans;
 }
 
+void mirror(node* root) {
+	// base case
+	if (!root) {
+		return;
+	}
+
+	swap(root->left, root->right);
+	mirror(root->left);
+	mirror(root->right);
+}
+
+node* searchBT(node* root, int key) {
+	// base case
+	if (!root) {
+		return NULL;
+	}
+	// recursive case
+	if (root->data == key) {
+		return root;
+	}
+
+	node* left_ans = searchBT(root->left, key);
+	if (left_ans != NULL) {
+		return left_ans;
+	}
+	return searchBT(root->right, key);
+}
+
+void levelOrder(node* root) {
+
+	queue<node*> q;
+	q.push(root);
+	q.push(NULL);
+	while (!q.empty()) {
+		node* x = q.front();
+		q.pop();
+
+		if (x != NULL) {
+			cout << x->data << " ";
+			if (x->left) q.push(x->left);
+			if (x->right) q.push(x->right);
+		}
+		else {
+			cout << endl;
+			if (!q.empty()) {
+				q.push(NULL);
+			}
+		}
+	}
+}
+
+node* levelOrderInput() {
+	node* root = NULL;
+
+	int data, l, r;
+	cout << "Enter the data of root: ";
+	cin >> data;
+	root = new node(data);
+	queue<node*> q;
+	q.push(root);
+	while (!q.empty()) {
+		node* x = q.front();
+		q.pop();
+		cout << "Enter left and right child of: " << x->data << endl;
+		cin >> l >> r;
+		if (l != -1) {
+			x->left = new node(l);
+			q.push(x->left);
+		}
+		if (r != -1) {
+			x->right = new node(r);
+			q.push(x->right);
+		}
+	}
+
+	return root;
+}
+
+int pre[] = {8, 10, 1, 6, 4, 7, 3, 14, 13};
+int in[] = {1, 10, 4, 6, 7, 8, 3, 13, 14};
+int i = 0;
+node* buildTree(int s, int e) {
+	if (s > e) {
+		return NULL;
+	}
+	node* root = new node(pre[i++]);
+
+	int k = -1;
+	for (int j = s; j <= e; ++j)
+	{
+		if (root->data == in[j]) {
+			k = j;
+			break;
+		}
+	}
+	root->left = buildTree(s, k - 1);
+	root->right = buildTree(k + 1, e);
+	return root;
+}
+
 int main() {
 
 	node* root = NULL;
-	root = createTree();
-
+	// root = createTree();
+	// root = levelOrderInput();
+	root = buildTree(0, (sizeof(pre) / sizeof(int)) - 1);
+	// mirror(root);
 	preorder(root);
 	cout << endl;
 	inorder(root);
@@ -135,6 +238,15 @@ int main() {
 	Pair ans = fastDiameter(root);
 	cout << "Fast Diameter: " << ans.dia << endl;
 	cout << "Fast Height: " << ans.height << endl;
+
+	node* s = searchBT(root, 100);
+	if (s) {
+		cout << s->data << endl;
+	}
+	else {
+		cout << "Key Not Found" << endl;
+	}
+	levelOrder(root);
 
 	return 0;
 }
